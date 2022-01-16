0e829892ffd6d6ed99bb06a9d59b4f6c4a0ca978
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-16 17:43:06 +0700

    text button

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,12 +9,57 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        width: Get.width,
-        child: Image.asset(
-          "assets/images/bg.png",
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        children: [
+          Container(
+            width: Get.width,
+            child: Image.asset(
+              "assets/images/bg.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(
+                    "assets/images/pict_1.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Text("Welcome to"),
+                Text("Dirrbox"),
+                Container(
+                  width: 200,
+                  child: Text(
+                    "Best cloud storage platform for all business and individuals to manage there data \n\nJoin For Free.",
+                  ),
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset("assets/images/fingerprint.png"),
+                          Text("SmartId"),
+                        ],
+                      ),
+                      style: TextButton.styleFrom(primary: Color(0xFF567DF4)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
