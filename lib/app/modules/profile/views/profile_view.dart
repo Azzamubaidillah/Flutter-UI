a911b257fc04393be8fdd07a90935b4a32c9ed46
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-20 00:21:38 +0700

    card profile

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -8,113 +8,114 @@ class ProfileView extends GetView<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          leading: Padding(
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Color(0xFF22215B),
+            ),
+          ),
+        ),
+        title: Text(
+          'My Profile',
+          style: TextStyle(
+            color: Color(0xFF22215B),
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: IconButton(
               onPressed: () {},
               icon: Icon(
-                Icons.arrow_back_ios,
+                Icons.menu,
                 color: Color(0xFF22215B),
               ),
             ),
           ),
-          title: Text(
-            'My Profile',
-            style: TextStyle(
-              color: Color(0xFF22215B),
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          actions: [
+        ],
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.menu,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Container(
+                width: Get.width,
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(width: 40),
+                          Image.asset("assets/images/profile.png"),
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              child: Text(
+                                "PRO",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFF317B),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          "Neelesh Chaudhary",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          "UI/UX Designer",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare pretium placerat ut platea.",
+                        style: TextStyle(
+                          color: Colors.white60,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
                   color: Color(0xFF22215B),
                 ),
               ),
             ),
           ],
-          centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Container(
-                  width: Get.width,
-                  height: 200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            SizedBox(width: 40),
-                            Image.asset("assets/images/profile.png"),
-                            Container(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 5),
-                                child: Text(
-                                  "PRO",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF317B),
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: Text(
-                            "Neelesh Chaudhary",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 5),
-                          child: Text(
-                            "UI/UX Designer",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare pretium placerat ut platea.",
-                          style: TextStyle(
-                            color: Colors.white60,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xFF22215B),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
