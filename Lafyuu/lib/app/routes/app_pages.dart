416313aedcfd303d892ace165c6d4b8a551d52bb
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-21 09:08:53 +0700

    splash screen init

--- Lafyuu/lib/app/routes/app_pages.dart
+++ Lafyuu/lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import 'package:lafyuu/app/modules/home/bindings/home_binding.dart';
 import 'package:lafyuu/app/modules/home/views/home_view.dart';
+import 'package:lafyuu/app/modules/splash_screen/bindings/splash_screen_binding.dart';
+import 'package:lafyuu/app/modules/splash_screen/views/splash_screen_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH_SCREEN,
+      page: () => SplashScreenView(),
+      binding: SplashScreenBinding(),
     ),
   ];
 }
--- Lafyuu/lib/app/routes/app_routes.dart
+++ Lafyuu/lib/app/routes/app_routes.dart
@@ -5,8 +5,10 @@ abstract class Routes {
   Routes._();
 
   static const HOME = _Paths.HOME;
+  static const SPLASH_SCREEN = _Paths.SPLASH_SCREEN;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const SPLASH_SCREEN = '/splash-screen';
 }
