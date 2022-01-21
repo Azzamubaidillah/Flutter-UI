6a25774d2506a0fcf3b511774b128c620683633a
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-21 09:31:33 +0700

    const

---
+++ Lafyuu/lib/const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const primaryBlue = Color(0xFF40BFFF);
+const primaryRed = Color(0xFFFB7181);
+const primaryYellow = Color(0xFFFFC833);
+const primaryGreen = Color(0xFF53D1B6);
+const primaryPurple = Color(0xFF5C61F4);
+
+const dark = Color(0xFF223263);
+const grey = Color(0xFF9098B1);
+const light = Color(0xFFEBF0FF);
+
+const double kDefaultPadding = 16.0;
