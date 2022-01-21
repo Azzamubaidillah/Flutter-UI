49234a756e4839f7006f0ba5a37ca578c6801230
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-01-21 10:33:31 +0700

    fix textTheme

--- Lafyuu/lib/app/modules/login/views/login_view.dart
+++ Lafyuu/lib/app/modules/login/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lafyuu/const.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -8,14 +9,27 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Image.asset("assets/icons/logo_blue.png"),
+                Text(
+                  "Welcome to Lafyuu",
+                  style: Theme.of(context).textTheme.headline4!.merge(
+                        TextStyle(color: dark),
+                      ),
+                ),
+                Text(
+                  "Sign in to continue",
+                  style: Theme.of(context).textTheme.bodyText1!.merge(
+                        TextStyle(color: grey),
+                      ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
--- Lafyuu/lib/main.dart
+++ Lafyuu/lib/main.dart
@@ -15,16 +15,51 @@ void main() {
       theme: ThemeData(
         primaryColor: bluePrimary,
         textTheme: TextTheme(
-          headline1: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
-          headline2: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-          headline3: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-          headline4: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-          headline5: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-          headline6: TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
-          bodyText1: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
-          caption: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+          headline1: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          headline2: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          headline3: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          headline4: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          headline5: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          headline6: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
+          bodyText1: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              height: 1.5,
+              letterSpacing: 0.5),
+          caption: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.w400,
+              height: 1.5,
+              letterSpacing: 0.5),
           button: TextStyle(
-              fontSize: 14, fontWeight: FontWeight.w700, letterSpacing: 0.5),
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+              height: 1.5,
+              letterSpacing: 0.5),
         ),
       ),
     ),
