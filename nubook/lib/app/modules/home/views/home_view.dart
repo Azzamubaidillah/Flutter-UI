d0dc0c9b0fa158f16839c0ddd6158e65e85b6f23
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-03-02 19:33:46 +0700

    recent play section

--- nubook/lib/app/modules/home/views/home_view.dart
+++ nubook/lib/app/modules/home/views/home_view.dart
@@ -17,17 +17,7 @@ class HomeView extends GetView<HomeController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: kPadding / 2),
-                child: Text(
-                  "Daily Recommended",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 24,
-                    color: kBlack,
-                  ),
-                ),
-              ),
+              Title(title: "Recommended"),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
@@ -45,11 +35,141 @@ class HomeView extends GetView<HomeController> {
                       title: "Dibalik Investigasi\nTempo",
                       subtitle: "Lorem ipsum dolor sit amet",
                     ),
+                    SizedBox(width: 15),
                   ],
                 ),
-              )
+              ),
+              TitleWithButton(
+                title: "Recently Played",
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      image: "assets/images/image_3.png",
+                      title: "Manusia Setengah Dewa",
+                      artist: "Iwan Fals",
+                    ),
+                    SizedBox(width: 10),
+                    Card(
+                      image: "assets/images/image_4.png",
+                      title: "Tanpa Karena",
+                      artist: "Fiersa Besari",
+                    ),
+                    SizedBox(width: 10),
+                    Card(
+                      image: "assets/images/image_5.png",
+                      title: "Sahabat Sejati Paling Setia",
+                      artist: "Sheila on 7",
+                    ),
+                    SizedBox(width: 10),
+                  ],
+                ),
+              ),
+              TitleWithButton(
+                title: "My Campaign",
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.artist,
+  }) : super(key: key);
+
+  final String image, title, artist;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 130,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset(
+              image,
+              width: 130,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            artist,
+            style: TextStyle(
+                fontSize: 13,
+                color: kBlack.withOpacity(0.6),
+                fontWeight: FontWeight.w500),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithButton extends StatelessWidget {
+  const TitleWithButton({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.fromLTRB(0, kPadding / 2, kPadding, kPadding / 2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Title(title: title),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "See All",
+              style: TextStyle(
+                  fontSize: 14, fontWeight: FontWeight.w500, color: kPrimary),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kPadding / 2),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          fontSize: 22,
+          color: kBlack,
         ),
       ),
     );
