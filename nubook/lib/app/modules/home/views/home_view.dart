b7be015bc98bb2a498cc6a4ebcfedc1a74a96872
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-03-02 17:36:37 +0700

    recommended card

--- nubook/lib/app/modules/home/views/home_view.dart
+++ nubook/lib/app/modules/home/views/home_view.dart
@@ -13,7 +13,7 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kPadding),
+          padding: const EdgeInsets.fromLTRB(kPadding, 0, 0, 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -28,45 +28,95 @@ class HomeView extends GetView<HomeController> {
                   ),
                 ),
               ),
-              Row(
-                children: [
-                  Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          "assets/images/image_1.png",
-                          width: 240,
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 12, vertical: 6),
-                              child: Text(
-                                "Novel",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                              color: kPrimary,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(15),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    RecommendedCard(
+                      image: "assets/images/image_1.png",
+                      tag: "Novel",
+                      title: "Sabtu Berasama\nBapak",
+                      subtitle: "Lorem ipsum dolor sit amet",
+                    ),
+                    SizedBox(width: 15),
+                    RecommendedCard(
+                      image: "assets/images/image_2.png",
+                      tag: "Investigasi",
+                      title: "Dibalik Investigasi\nTempo",
+                      subtitle: "Lorem ipsum dolor sit amet",
+                    ),
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class RecommendedCard extends StatelessWidget {
+  const RecommendedCard({
+    Key? key,
+    required this.image,
+    required this.tag,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  final String image, tag, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image.asset(
+            image,
+            width: 240,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: kPadding / 1.5, vertical: kPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  child: Text(
+                    tag,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: kPrimary,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                title,
+                style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              Text(
+                subtitle,
+                style: TextStyle(fontSize: 11, color: Colors.white),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
